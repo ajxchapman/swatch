a83de7f5a4fcf0d06368087b5a25db2f101066cd
Alex Chapman <ajxchapman@gmail.com>
2021-12-20 14:54:46 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -2,10 +2,10 @@ import argparse
 import os
 import requests
 
-def get_cache():
+def get_cache(filename):
     r = requests.get(
-        f"{os.environ['CI_API_V4_URL']}/projects/{os.environ['CI_PROJECT_ID']}/jobs/artifacts/{os.environ['CI_COMMIT_REF_NAME']}/raw/cache.yaml", 
-        params={"job" : os.environ['CI_BUILD_STAGE'] : "job_token" : os.environ['CI_JOB_TOKEN']}
+        f"{os.environ['CI_API_V4_URL']}/projects/{os.environ['CI_PROJECT_ID']}/jobs/artifacts/{os.environ['CI_COMMIT_REF_NAME']}/raw/{filename}", 
+        params={"job" : os.environ['CI_BUILD_STAGE'], "job_token" : os.environ['CI_JOB_TOKEN']}
     )
     print(r.status_code)
     print(r.body)
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     parser.add_argument("--output", "-o", type=str, default="output.yaml")
     parser.add_argument("--input", "-i", type=str, default="watch.yaml")
     args = parser.parse_args()
-    get_cache()
+    get_cache(args.output)
     with open(parser.output, "w") as f:
         print(os.environ["CI_JOB_ID"])
         f.write(os.environ["CI_JOB_ID"])

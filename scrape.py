d762b4b6e60fd92fb0d5bc10cc16dbe1d9b07807
Alex Chapman <ajxchapman@gmail.com>
2021-12-20 15:03:18 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,18 +1,21 @@
 import argparse
 import os
 import requests
+import yaml
 
 def read_cache(filename):
     r = requests.get(
         f"{os.environ['CI_API_V4_URL']}/projects/{os.environ['CI_PROJECT_ID']}/jobs/artifacts/{os.environ['CI_COMMIT_REF_NAME']}/raw/{filename}", 
         params={"job" : os.environ['CI_BUILD_STAGE'], "job_token" : os.environ['CI_JOB_TOKEN']}
     )
+    print(r.url)
     print(r.status_code)
     print(r.text)
     return r.text
 
 def write_cache(obj, filename):
-    pass
+    with open(args.output, "w") as f:
+        yaml.dump(obj, f, default_flow_style=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,6 +24,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     read_cache(args.output)
-    with open(args.output, "w") as f:
-        print(os.environ["CI_JOB_ID"])
-        f.write(os.environ["CI_JOB_ID"])
+    write_cache({"job_id" : os.environ["CI_JOB_ID"]}, args.output)
+    

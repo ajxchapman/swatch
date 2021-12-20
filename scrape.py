1bc43c065e5e012932c90c85cdb8ac6839969643
Alex Chapman <ajxchapman@gmail.com>
2021-12-20 15:20:53 +0000

    Update scrape.py, .gitlab-ci.yml

--- scrape.py
+++ scrape.py
@@ -8,21 +8,18 @@ def read_cache(filename):
         f"{os.environ['CI_API_V4_URL']}/projects/{os.environ['CI_PROJECT_ID']}/jobs/artifacts/{os.environ['CI_COMMIT_REF_NAME']}/raw/{filename}", 
         params={"job" : os.environ['CI_BUILD_STAGE'], "job_token" : os.environ['CI_JOB_TOKEN']}
     )
-    print(r.url)
-    print(r.status_code)
-    print(r.text)
     return r.text
 
 def write_cache(obj, filename):
-    with open(args.output, "w") as f:
+    with open(filename, "w") as f:
         yaml.dump(obj, f, default_flow_style=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--output", "-o", type=str, default="output.yaml")
+    parser.add_argument("--cache", "-c", type=str, default="cache.yaml")
     parser.add_argument("--input", "-i", type=str, default="watch.yaml")
     args = parser.parse_args()
 
-    read_cache(args.output)
-    write_cache({"job_id" : os.environ["CI_JOB_ID"]}, args.output)
+    read_cache(args.cache)
+    write_cache({"job_id" : os.environ["CI_JOB_ID"]}, args.cache)
     

88fbf9bddec3ece49d27acc64cdb04b1f6af97b6
Alex Chapman <3729326+ajxchapman@users.noreply.github.com>
2022-07-06 15:16:50 +0000

    Webhook alerting

--- watch.py
+++ watch.py
@@ -15,17 +15,17 @@ from src.context import Context
 from src.watch import Watch, WatchException, render_comment
 
 
-# def alert(message, alert_hook=None):
-#     if alert_hook is not None:
-#         data = alert_hook.get("payload", json.dumps({"text" : message})).replace("MESSAGE", json.dumps(message))
-#         r = requests.request(
-#             alert_hook.get("method", "POST"),
-#             alert_hook["url"],
-#             headers={"content-type" : alert_hook.get("content-type", "application/json")},
-#             data=data.encode()
-#         )
-#     else:
-#         print(message)
+def alert(message, alert_hook=None):
+    if alert_hook is not None:
+        data = alert_hook.get("payload", json.dumps({"text" : message})).replace("MESSAGE", json.dumps(message))
+        r = requests.request(
+            alert_hook.get("method", "POST"),
+            alert_hook["url"],
+            headers={"content-type" : alert_hook.get("content-type", "application/json")},
+            data=data.encode()
+        )
+    else:
+        print(message)
 
 def process(config, cache, verbose=False):
     watches = [Watch.load(**x) for x in config.get("watch", [])]
@@ -40,7 +40,7 @@ def process(config, cache, verbose=False):
             ctx.set_variable("tmpdir", tmpdir)
             try:
                 if watch.match_data(ctx, watch.process_data(ctx)):
-                    print(render_comment(watch.get_comment(ctx)))
+                    alert(render_comment(watch.get_comment(ctx)), config["config"].get("hook"))
             except WatchException as e:
                 key = e.key
                 if not verbose:
@@ -58,20 +58,27 @@ def replace_var(vars, var):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cache", "-c", type=str, default="cache.yaml")
-    parser.add_argument("--input", "-i", type=str, default="watch.yaml")
+    parser.add_argument("--input", "-i", action="append")
     parser.add_argument("--verbose", "-v", action="store_true")
     parser.add_argument("--test", "-t", action="store_true")
     args = parser.parse_args()
 
-    config = {}
-    for watch_file in glob.glob(args.input, recursive=True):
+    config = {
+        "watch": [],
+        "config": {}
+    }
+    for watch_file in set([x for xs in [glob.glob(y, recursive=True) for y in args.input] for x in xs]):
         with open(watch_file) as f:
             watch_data = f.read()
     
         watch_config = yaml.safe_load(watch_data)
         watch_data = re.sub(r'\$\{([^}]+)\}', lambda match: replace_var(watch_config.get("variables", {}), match.group(1)), watch_data)
         watch_config = yaml.safe_load(watch_data)
-        config.setdefault("watch", []).extend(watch_config["watch"])
+        config["watch"].extend(watch_config.get("watch", []))
+
+        if len(config["config"].keys() & watch_config.get("config", {}).keys()) > 0:
+            raise Exception(f"Conflicting config keys {list(config['config'].keys() & watch_config['config'].keys())} in '{watch_file}'")
+        config["config"].update(watch_config.get("config", {}))
 
     if args.test:
         process(config, Cache(cache_path=None), verbose=args.verbose)

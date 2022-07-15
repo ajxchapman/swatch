195180dd610dce860646a920cdb382cdb71d5c99
Alex Chapman <3729326+ajxchapman@users.noreply.github.com>
2022-07-15 10:28:15 +0000

    Basic conditional match

--- src/match.py
+++ src/match.py
@@ -1,9 +1,11 @@
 import hashlib
+import re
 import typing
 
 from src.loadable import Loadable
 from src.context import Context
 from src.cache import Cache
+from src.template import template_render
 
 class MatchException(Exception):
     pass
@@ -29,6 +31,48 @@ class CacheMatch(Match):
             cache.put_entry(key_digest, data_digest)
             return True
         return False
+
+class CondMatch(Match):
+    keys = {
+        "operator" : (str, None),
+        "value" : (lambda x: x, None),
+        "comparitor" : (str, "{{ data }}")
+    }
+    default_key = "value"
+    operators = {"eq" : "eq", "==" : "eq", "lt" : "lt", "<" : "lt", "lte" : "lte", "<=" : "lte", "gt" : "gt", ">" : "gt", "gte" : "gte", ">=" : "gte"}
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        m = re.match("(?:(.*)\s+)?(eq|==|lt|<|lte|<=|gt|>|gte|>=)\s+(.*)", self.value)
+        if m is not None:
+            self.comparitor, self.operator, self.value = m.groups()
+            self.comparitor = self.comparitor or self.keys["comparitor"][1]
+        
+        self.operator = self.operators.get(self.operator)
+        if self.operator is None:
+            raise MatchException(f"Unknown operator '{self.operator}")
+
+    def match(self, ctx: Context, data: typing.List[bytes]) -> bool:
+        c1 = template_render(self.comparitor, ctx.variables, data=data)
+        c2 = template_render(self.value, ctx.variables, data=data)
+
+        if self.operator != "eq":
+            try:
+                c1 = int(c1)
+                c2 = int(c2)
+            except (TypeError, ValueError):
+                raise MatchException("Less than and Greater than comparisons can only be performed on integer values")
+        
+        if self.operator == "lt":
+            return c1 < c2
+        elif self.operator == "lte":
+            return c1 <= c2
+        elif self.operator == "gt":
+            return c1 > c2
+        elif self.operator == "gte":
+            return c1 >= c2
+        return c1 == c2
 
 class NoneMatch(Match):
     """
---
+++ tests/match/test_cond.py
@@ -0,0 +1,44 @@
+import unittest
+
+from src.match import Match, MatchException
+from src.context import Context
+
+class TestCondMatch(unittest.TestCase):
+    def setUp(self) -> None:
+        self.ctx = Context()
+        self.ctx.set_variable("v1", 123)
+        self.ctx.set_variable("i2", 456)
+    
+    def test_lt(self):
+        m: Match = Match.load(cond="lt 10")
+        self.assertEqual(m.match(self.ctx, [b'1']), True)
+        self.assertEqual(m.match(self.ctx, [b'11']), False)
+
+    def test_lt_exception(self):
+        m: Match = Match.load(cond="lt 10")
+        with self.assertRaises(MatchException):
+            m.match(self.ctx, [b'abc'])
+
+        m: Match = Match.load(cond="lt abc")
+        with self.assertRaises(MatchException):
+            m.match(self.ctx, [b'10'])
+    
+    def test_eq_int(self):
+        m: Match = Match.load(cond="eq 10")
+        self.assertEqual(m.match(self.ctx, [b'10']), True)
+        self.assertEqual(m.match(self.ctx, [b'11']), False)
+    
+    def test_eq_str(self):
+        m: Match = Match.load(cond="eq abc")
+        self.assertEqual(m.match(self.ctx, [b'abc']), True)
+        self.assertEqual(m.match(self.ctx, [b'def']), False)
+
+    def test_eq_static(self):
+        m: Match = Match.load(cond="1 eq 1")
+        self.assertEqual(m.match(self.ctx, [b'abc']), True)
+        self.assertEqual(m.match(self.ctx, [b'def']), True)
+
+    def test_eq_ctx(self):
+        m: Match = Match.load(cond="eq {{ v1 }}")
+        self.assertEqual(m.match(self.ctx, [b'123']), True)
+        self.assertEqual(m.match(self.ctx, [b'456']), False)

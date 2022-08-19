dc14ea01a87827f80b4cbf4bdb60619f294ca44d
Alex Chapman <3729326+ajxchapman@users.noreply.github.com>
2022-08-19 15:02:31 +0000

    Refactor main loop to use a single context per file Plus variables, befores and afters

--- watch.py
+++ watch.py
@@ -26,33 +26,49 @@ def alert(message, alert_hook=None):
     else:
         print(message)
 
-def process(config, cache, verbose=False):
-    watches = [Watch.load(**x) for x in config.get("watch", [])]
+def process(config, cache, watch_files, verbose=False):
     cwd = os.getcwd()
-
-    for watch in watches:
-        # Create a new context and temporary working directory for each watch
+    for watch_file in watch_files:
+        with open(watch_file) as f:
+            watch_config = yaml.safe_load(f)
+        
         ctx = Context()
         ctx.set_variable("cache", cache)
+         # Load variables into the context
+        for k, v in watch_config.get("variables", {}).items():
+            ctx.set_variable(k, ctx.expand_context(v))
 
         try:
             with tempfile.TemporaryDirectory() as tmpdir:
                 os.chdir(tmpdir)
                 ctx.set_variable("tmpdir", tmpdir)
-                try:
-                    if watch.process(ctx):
-                        print(f"{watch.hash}:True")
-                        alert(render_comment(watch.get_comment(ctx)), config["config"].get("hook"))
-                    else:
-                        print(f"{watch.hash}:False")
-                except WatchException as e:
-                    key = e.key
-                    print(f"Error processing {key}:\n\t{e.__class__.__name__}: {e}")
-                    traceback.print_tb(e.__traceback__, file=sys.stdout)
-                finally:
-                    os.chdir(cwd)
+
+                # Load an execute any 'before' tasks
+                for before in [Watch.load(**x) for x in watch_config.get("before", [])]:
+                    before.process(ctx)
+                
+                # Execute main 'watch' tasks
+                for watch in [Watch.load(**x) for x in watch_config.get("watch", [])]:
+                    try:
+                        if watch.process(ctx):
+                            print(f"{watch.hash}:True")
+                            alert(render_comment(watch.get_comment(ctx)), config.get("hook"))
+                        else:
+                            print(f"{watch.hash}:False")
+                    except WatchException as e:
+                        print(f"Error processing {e.hash}:\n\t{e.__class__.__name__}: {e}")
+                        traceback.print_tb(e.__traceback__, file=sys.stdout)
+
+                        # Early exit out of a watch_file in the event on an exception
+                        break
+                
+                # Load an execute any 'after' tasks
+                for after in [Watch.load(**x) for x in watch_config.get("after", [])]:
+                    after.process(ctx)
         except PermissionError:
             print(f"Error removing temporary directory\n")
+        finally:
+            os.chdir(cwd)
 
 def replace_var(vars, var):
     if var in vars:
@@ -61,34 +77,25 @@ def replace_var(vars, var):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cache", "-c", type=str, default="cache.yaml")
+    parser.add_argument("--cache", "-c", type=str, default="cache.tar.gz")
+    parser.add_argument("--config", type=str, default="watches/conf.yml")
     parser.add_argument("--verbose", "-v", action="store_true")
     parser.add_argument("--test", "-t", action="store_true")
     parser.add_argument('watches', nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
-    config = {
-        "watch": [],
-        "config": {}
-    }
-    for watch_file in set([x for xs in [glob.glob(y, recursive=True) for y in args.watches] for x in xs]):
-        with open(watch_file) as f:
-            watch_data = f.read()
-    
-        watch_config = yaml.safe_load(watch_data)
-        watch_data = re.sub(r'\$\{([^}]+)\}', lambda match: replace_var(watch_config.get("variables", {}), match.group(1)), watch_data)
-        watch_config = yaml.safe_load(watch_data)
-        config["watch"].extend(watch_config.get("watch", []))
-
-        if len(config["config"].keys() & watch_config.get("config", {}).keys()) > 0:
-            raise Exception(f"Conflicting config keys {list(config['config'].keys() & watch_config['config'].keys())} in '{watch_file}'")
-        config["config"].update(watch_config.get("config", {}))
+    config = {}
+    if not (args.test):
+        if os.path.isfile(args.config):
+            with open(args.config) as f:
+                config = yaml.safe_load(f).get("config", {})
+    watch_files = set([x for xs in [glob.glob(y, recursive=True) for y in args.watches] for x in xs])
 
     if args.test:
-        process(config, Cache(cache_path=None), verbose=args.verbose)
+        process(config, Cache(cache_path=None), watch_files, verbose=args.verbose)
     else:
         cache = Cache(cache_path=args.cache, encryption_key=config["config"].get("key"))
         try:
-            process(config, cache, verbose=args.verbose)
+            process(config, cache, watch_files, verbose=args.verbose)
         finally:
             cache.close()

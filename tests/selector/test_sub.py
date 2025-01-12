dc8bf589c6b3700999421253fd54a91b97f1ce98
ajxchapman <3729326+ajxchapman@users.noreply.github.com>
2025-01-12 14:49:54 +0000

    Fix sub test

--- tests/selector/test_sub.py
+++ tests/selector/test_sub.py
@@ -2,7 +2,7 @@ import unittest
 import yaml
 
 from src.context import Context
-from src.selector import Selector
+from src.selector import Selector, SelectorItem
 
 
 
@@ -19,10 +19,10 @@ sub:
   - join: ','
 """
         s: Selector = Selector.load(**yaml.safe_load(test_schema))
-        data = [b'1,2,3,4,5', b'6,7,8,9,10']
+        data = [SelectorItem(b'1,2,3,4,5'), SelectorItem(b'6,7,8,9,10')]
 
         result = s.execute(self.ctx, data)
-        self.assertListEqual(result, [b'2,3', b'7,8'])
+        self.assertListEqual([x.value for x in result], [b'2,3', b'7,8'])
 
     def test_returns_multiple(self):
         test_schema = """
@@ -31,7 +31,7 @@ sub:
   - pick: [1, 2]
 """
         s: Selector = Selector.load(**yaml.safe_load(test_schema))
-        data = [b'1,2,3,4,5', b'6,7,8,9,10']
+        data = [SelectorItem(b'1,2,3,4,5'), SelectorItem(b'6,7,8,9,10')]
 
         result = s.execute(self.ctx, data)
-        self.assertListEqual(result, [b'2', b'3', b'7', b'8'])
+        self.assertListEqual([x.value for x in result], [b'2', b'3', b'7', b'8'])
